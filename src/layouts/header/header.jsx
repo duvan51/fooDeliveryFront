f609cbbbff0b17e4319bcc14aaa9d56a12c85946
duvan51 <duvanaponteramirez@gmail.com>
2025-01-17 02:12:37 -0500

    arreglado header commit

--- src/layouts/header/header.jsx
+++ src/layouts/header/header.jsx
@@ -6,9 +6,9 @@ import Login from "../../pages/login/login";
 import RegisterUser from "../../pages/register/registerUser";
 import { AuthContext } from "../../context/AuthContext";
 
-import ModalCart from "../../pages/modalCart/modalCart";
+import ModalCart from "../../pages/modalCart/modalCart.jsx";
 
-const header = () => {
+const Header = () => {
   const [activeDiv, setActiveDiv] = useState(null);
   const userData = JSON.parse(localStorage.getItem("usuario"));
 
@@ -144,4 +144,4 @@ const header = () => {
   );
 };
 
-export default header;
+export default Header;
